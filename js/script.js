19bcd4e7d7eec300028ff0c4816cffeeffade1d9
Mattia-Giorgetti <mattiagiorgetti37@gmail.com>
2022-10-21 10:27:17 +0200

    aggiunte immagini e stile

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// FUNZIONI 
+
 function stampaConsole(){
 
     for(let value of team){
@@ -7,63 +9,67 @@ function stampaConsole(){
              console.log(key + ': ' +  value[key]);
         }
     }
-
 }
 
 function creaCard(){
-    for(let i = 0; i < membersNum; i++){
+
+    for(let i = 0; i < team.length; i++){
+
         let myCard = document.createElement('div');
-        let cardImg = document.createElement('div');
+        let cardImg = document.createElement('img');
+        cardImg.src = 'img/' + team[i].image;
         let cardName = document.createElement('div');
+        cardName.classList.add('mb-3', 'fw-bolder', 'text-white','fs-3');
         let cardRole = document.createElement('div');
-        myCard.classList.add('css_card', 'col-lg-3','m-auto','my_flex', 'mb-5');
+        myCard.classList.add('css_card','col-lg-3','m-auto','my_flex', 'mb-5');
         myContainer.append(myCard);
-            myCard.append(cardImg)
-            myCard.append(cardName)
-            myCard.append(cardRole)
-            cardImg.append(team[i].image)
-            cardName.append(team[i].nome)
-            cardRole.append(team[i].ruolo)
+        myCard.append(cardImg);
+        myCard.append(cardName);
+        myCard.append(cardRole);
+        cardImg.append(team[i].image);
+        cardName.append(team[i].nome);
+        cardRole.append(team[i].ruolo);
     }
 }
 
-
+// VARIABILI 
 
 const myContainer = document.getElementById('container');
-const membersNum = 6;
 
 const team = [
     {
         nome: 'Wayne Barnett',
         ruolo: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpeg',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         nome: 'Angela Caroll',
         ruolo: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpeg',
+        image: 'angela-caroll-chief-editor.jpg',
 
     },
     {
         nome: 'Walter Gordon',
         ruolo: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpeg',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
         nome: 'Angela Lopez',
         ruolo: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpeg',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
         nome: 'Scott Estrada',
         ruolo: 'Developer',
-        image: 'scott-estrada-developer.jpeg',
+        image: 'scott-estrada-developer.jpg',
     },
     {
         nome: 'Barbara Ramos',
         ruolo: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpeg',
+        image: 'barbara-ramos-graphic-designer.jpg',
     }
 ];
+
+// INVOCO LE FUNZIONI 
 stampaConsole();
 creaCard();
